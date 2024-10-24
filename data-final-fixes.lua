452625797b18d8a164555037f930322c0f361945
Kubius <12454065+Kubius@users.noreply.github.com>
2024-10-24 09:25:37 -0500

    0.4.3

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,16 +1,41 @@
--- Remove restrictions on concrete placement
+-- Remove most restrictions on concrete placement
+
+data:extend({{type = "collision-layer", name = "ref_concrete_valid"}})
+
+local valid_concrete_targets = {
+    "water", "deepwater", "water-green", "deepwater-green", -- all water
+    "water-mud", "water-shallow", -- all shallows
+    "wetland-light-green-slime", "wetland-green-slime", "wetland-light-dead-skin", "wetland-dead-skin", "wetland-pink-tentacle", "wetland-red-tentacle", "wetland-yumako", "wetland-jellynut",
+    "oil-ocean-shallow" -- shallow oil ocean (deep is a bit too much to stretch to)
+    -- no lava or ammonia
+}
+
+for _,tile in pairs(data.raw.tile) do 
+    local mask = tile.collision_mask.layers
+    if mask["ground_tile"] then
+        mask["ref_concrete_valid"] = true
+    else
+        for _,concrete_target in pairs(valid_concrete_targets) do
+            if(tile.name == concrete_target) then
+                mask["ref_concrete_valid"] = true
+            end
+        end
+    end
+end
+
 data.raw.item["refined-concrete"].place_as_tile = {
     result = "refined-concrete",
     condition_size = 1,
-    condition = {layers = {}}
+    condition = {layers={["ref_concrete_valid"]=true}},
+    invert = true
 }
 
 data.raw.item["refined-hazard-concrete"].place_as_tile = {
     result = "refined-hazard-concrete-left",
     condition_size = 1,
-    condition = {layers = {}}
+    condition = {layers={["ref_concrete_valid"]=true}},
+    invert = true
 }
-
 
 -- If the resource prevention hasn't been removed by Water Ores, do it ourselves
 if not mods["Water_Ores"] then
@@ -35,7 +60,7 @@ if settings.startup["fish_safety"].value then
     end
 end
 
--- Dramatically reduce refined concrete yield to make sea building more expensive
+-- If desired, dramatically reduce refined concrete yield to make sea building more expensive
 if settings.startup["nerf_concrete"].value > 0 then
     data.raw.recipe["refined-concrete"].results[1].amount = settings.startup["nerf_concrete"].value
 end
